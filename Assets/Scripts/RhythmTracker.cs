e9848f54cd2ec657011d87953445af923f3fc080
Hwang2442 <68269611+Hwang2442@users.noreply.github.com>
2023-10-27 00:31:40 +0900

    Feat: Onset 알고리즘 테스트
    - 이전 프레임과 비교하여 임계값을 넘을 시 해당 부분 저장
    - 테스트한 결과 비트 인식에 오랜 시간이 소요되어 사용 불가능
    - 알고리즘 최적화 및 고도화 필요한 것으로 판단됨

--- Assets/Scripts/RhythmTracker.cs
+++ Assets/Scripts/RhythmTracker.cs
@@ -1,17 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace RhythmEngine
 {
+    [RequireComponent(typeof(AudioSource))]
     public class RhythmTracker : MonoBehaviour
     {
-        [SerializeField] AudioSource m_source;
+        private AudioSource m_source;
 
+        [SerializeField] float threshold = 0.01f;
+        [SerializeField] int frameSize = 1024;
 
+        public List<int> mySets = new List<int>();
 
+        private void Start()
+        {
+            m_source = GetComponent<AudioSource>();
 
+            var clip = m_source.clip;
+            var samples = new float[clip.samples * clip.channels];
+            m_source.clip.GetData(samples, 0);
 
-        public float BPM { get; private set; }
+            var previousFrame = new float[frameSize];
+            for (int i = 0; i < samples.Length - frameSize; i += frameSize)
+            {
+                float sumCurrentFrame = 0;
+                float sumPreviousFrame = 0;
+                for (int j = 0; j < frameSize; j++)
+                {
+                    sumCurrentFrame += Mathf.Abs(samples[i + j]);
+                }
+                for (int j = 0; j < frameSize; j++)
+                {
+                    sumPreviousFrame += Mathf.Abs(previousFrame[j]);
+                }
+
+                if ((sumCurrentFrame - sumPreviousFrame) > threshold)
+                {
+                    mySets.Add(i);
+                }
+
+                for (int j = 0; j < frameSize; j++)
+                {
+                    previousFrame[j] = samples[i + j];
+                }
+            }
+        }
     }
 }
